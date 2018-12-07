5352bd2e08f45fbe440171784f7b382b9b202648
Berenger Salmon <berenger.salmon@gmail.com>
2018-12-07 11:48:31 +0100

    Beautifuly code of components

--- front/src/js/components/vsCats.js
+++ front/src/js/components/vsCats.js
@@ -5,57 +5,40 @@ import * as type from '../actions/const'
 import { fetchRateCat } from '../actions/cat'
 import logo from '../../medias/cat-mash-logo.png'
 
-const logoStyle = {
-  zIndex: 10,
-  width: "200px"
-}
-
-const imgClass = "btn btn-light border p-2 img-fluid align-self-center justify-content-center mx-auto rounded"
-
-const imgStyle= {
-  maxWidth: "60%",
-  zIndex: 50
-}
-
-const stateButton = {
-  bottom: 0,
-  left: "50%",
-  transform: "translateX(-50%)",
-  borderRadius: "0"
-}
 
 const VsCats = ({ dispatch, vsCats }) => {
-
   return (
-    <div className="container-fluid p-0">
-      <div className="col-12 position-fixed" style={{ zIndex: 10 }}>
-        <img src={logo} className="d-block mx-auto" width="200px" style={{}}/>
+    <React.Fragment>
+      <div className="container-fluid p-0">
+        <div className="col-12 position-fixed" style={{ zIndex: 10 }}>
+          <img src={logo} className="d-block mx-auto" width="200px"/>
+        </div>
+        <div className="row no-gutters" style={{minHeight: "100vh"}}>
+            {
+              vsCats.map((cat, index) => (
+                <div key={cat.id} className={"col-md-6 d-flex py-4 " + (index === 0?"bg-light":"bg-white")}>
+                  <img
+                  src={cat.url}
+                  onClick={() => { dispatch(fetchRateCat(cat.id)) }}
+                  className="btn btn-light border p-2 img-fluid align-self-center justify-content-center mx-auto rounded"
+                  style={{ maxWidth: "60%", zIndex: 50 }}
+                  />
+                </div>
+              ))
+            }
+        </div>
       </div>
-      <div className="row no-gutters" style={{minHeight: "100vh"}}>
-          {
-            vsCats.map((cat, index) => (
-              <div key={cat.id} className={"col-md-6 d-flex py-4 " + (index === 0?"bg-light":"bg-white")}>
-                <img
-                src={cat.url}
-                onClick={() => { dispatch(fetchRateCat(cat.id)) }}
-                className={imgClass}
-                style={imgStyle}
-                />
-              </div>
-            ))
-          }
-          <div
-            onClick={() => { dispatch({ type: type.STATS }) }}
-            className="btn btn-light rounded-top shadow border position-fixed py-2 px-5"
-            style={stateButton}>
-            <p className="m-0 text-dark">
-              Voir les plus beaux chats <br/>
-              xxx xxx votes
-            </p>
-          </div>
+      <div className="col-12 position-fixed" style={{ bottom: 0 }}>
+        <div
+          onClick={() => { dispatch({ type: type.HOME }) }}
+          className="btn btn-light d-block rounded-top shadow border mx-auto"
+          style={{ width: "300px" }}>
+          <p className="m-0 text-dark">
+            Retour aux votes
+          </p>
+        </div>
       </div>
-
-    </div>
+    </React.Fragment>
   )
 }
 
